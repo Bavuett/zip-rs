905464455e6c874dccde3a94a31edb13d8b1fabf
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2025-08-11 20:31:45 +0200

    🖊️ Flags Struct, Getters and Setters: First Draft.

--- src/archive/flags/mod.rs
+++ src/archive/flags/mod.rs
@@ -1,13 +1,13 @@
 mod implementation;
 
 pub struct Flags {
-    pub encrypted: bool,
-    pub reduced_compression: bool,
-    pub improved_compression: bool,
-    pub values_in_data_descriptor: bool,
-    pub enhanced_deflation: bool,
-    pub patched_data_compression: bool,
-    pub strong_encryption: bool,
-    pub utf8: bool,
-    pub central_directory_encryption: bool,
+    encrypted: bool,
+    reduced_compression: bool,
+    improved_compression: bool,
+    values_in_data_descriptor: bool,
+    enhanced_deflation: bool,
+    patched_data_compression: bool,
+    strong_encryption: bool,
+    utf8: bool,
+    central_directory_encryption: bool,
 }
